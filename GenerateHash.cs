a8cd3485f4573a3e0977d539a6bb0adb0eff3a7b
RushikeshAvhad <avhadrushikesh1804@gmail.com>
2024-06-11 22:07:50 +0530

    Take User Input and Convert To Hash

---
+++ GenerateHash.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace ConvertStringToHash
+{
+    public static class GenerateHash
+    {
+        public static string GenerateMD5Hash(string input)
+        {
+            // Create an instance of the MD5CryptoServiceProvider class
+            using (MD5 md5 = MD5.Create())
+            {
+                // Convert the input string to a byte array and compute the hash
+                byte[] inputBytes = Encoding.ASCII.GetBytes(input);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+                // Convert the byte array to a hexadecimal string
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < hashBytes.Length; i++)
+                {
+                    sb.Append(hashBytes[i].ToString("x2")); // "X2" for uppercase hex, "x2" for lowercase
+                }
+
+                return sb.ToString();
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
-﻿public class Program
+﻿using ConvertStringToHash;
+
+public class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Enter your string to Convert to Hash");
+        string input = Console.ReadLine();
+        string hash = GenerateHash.GenerateMD5Hash(input);
+        Console.WriteLine("The Hash value for {0} is {1} ", input, hash);
+        Console.ReadLine();
     }
 }
